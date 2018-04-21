92e35e850b92a0ec0ac533e8b76a0848f64b1614
unknown <henrik.peters.98@web.de>
2018-04-21 09:15:35 +0200

    added files to repo

---
+++ week 3/Aufg3.1.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 19 14:54:10 2018
+
+@author: 6peters
+"""
+
+import numpy as np
+from scipy import misc
+import matplotlib.pyplot as plt
+from skimage.io import imread, imsave
+from tabulate import tabulate
+
+d = np.load('./trainingsDaten2.npz')
+trImgs = d['data']
+trLabels = d['labels']
+
+valD = np.load('./validierungsDaten2.npz')
+valImgs = valD['data']
+valLabels = valD['labels']    
+
+
+
+def Aufg1():
+    trMeans = []
+    for count in range(trImgs.shape[0]):
+        trMeans.append(np.mean(trImgs[count,:,:]))
+        
+    valMeans = []
+    for count in range(valImgs.shape[0]):
+        valMeans.append(np.mean(valImgs[count,:,:]))
+    
+    labels = []
+    
+    for count in range(len(valMeans)):
+        dists = []    
+        for i in range(len(trMeans)):
+            dists.append(np.linalg.norm(trMeans[i]-valMeans[count]))
+        labels.append(trLabels[dists.index(min(dists))])
+    
+    return labels   
+
+print Aufg1()
+print np.sum(valLabels==Aufg1())
+print np.sum(valLabels==Aufg1()) / float(valImgs.shape[0]) * 100
+
+
+# Ziel: Erstmal besser als zufällige Zuweisung
+
+#Aufg2
+
+trHists = []
+valHists = []
+
+def hist_L2(hist1, hist2):
+    return np.linalg.norm(hist1-hist2)  
+    
+for count in range(trImgs.shape[0]):
+    trHists.append(np.histogram(trImgs[count,:,:], bins = 'sqrt')[0])
+
+for count in range(valImgs.shape[0]):
+    valHists.append(np.histogram(valImgs[count,:,:], bins = 'sqrt')[0])
+
+def Aufg2():
+        
+    labels = []
+    
+    for count in range(len(valHists)):
+        dists = []    
+        for i in range(len(trHists)):
+            dists.append(hist_L2(valHists[count], trHists[i]))
+        labels.append(trLabels[dists.index(min(dists))])
+    
+    return labels
+    
+    
+print np.sum(valLabels==Aufg2())
+print np.sum(valLabels==Aufg2()) / float(valImgs.shape[0]) * 100
+
+        
+#Aufg3
+
+def getWerte(i, Aufg):
+    if Aufg == 1: labels = Aufg1()
+    if Aufg == 2: labels = Aufg2()
+    auto = 0
+    hirsch = 0
+    schiff = 0
+    for count in range(len(valLabels)):
+        if valLabels[count] == i:
+            if labels[count] == 1:
+                auto+=1
+            elif labels[count] == 4:
+                hirsch+=1
+            else:
+                schiff+=1
+    return [auto, hirsch, schiff]
+                
+    #print tabulate([['eigtl. Auto', len([(valLabels==1 and labels1==1)]), 
+    #                                np.sum(valLabels==1 and labels1==4),
+    #                                np.sum(valLabels==1 and labels1==8)],
+    #                ['eigtl. Hirsch', np.sum(valLabels==4 and labels1==1),
+    #                                  np.sum(valLabels==4 and labels1==4),
+    #                                  np.sum(valLabels==4 and labels1==8)],
+    #                ['eigtl. Schiff', np.sum(valLabels==8 and labels1==1),
+    #                                 np.sum(valLabels==8 and labels1==4),
+    #                                  np.sum(valLabels==8 and labels1==8)]],
+    #                headers = ['Auto vgs.', 'Hirsch vgs.', 'Schiff vgs.'])
+                    
+
+print "Vorhersagen: Auto, Hirsch, Schiff"
+print "Eigentlich Auto:  ", getWerte(1,1)
+print "Eigentlich Hirsch:", getWerte(4,1)
+print "Eigentlich Schiff:", getWerte(8,1)        
+
+print "Vorhersagen: Auto, Hirsch, Schiff"
+print "Eigentlich Auto:  ", getWerte(1,2)
+print "Eigentlich Hirsch:", getWerte(4,2)
+print "Eigentlich Schiff:", getWerte(8,2)        
---
+++ week 3/Aufg3.2.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 20 15:40:51 2018
+
+@author: Moritz Lahann
+"""
+
+import numpy as np
+from scipy import misc
+import matplotlib.pyplot as plt
+from skimage.io import imread, imsave
+
+lenna = imread("./Lenna.png")
+
+
+#Aufg. 1
+
+#2. was ist mit zusammenfuegen gemeint?
+lennar = lenna[:,:,0]
+lennag = lenna[:,:,1]
+lennab = lenna[:,:,2]
+
+#5. Alle Werte werden invertiert
+lennainv = 255-lenna
+
+#3. heller -> farbe staerker vorhanden
+fig, ax = plt.subplots(2, 3)
+
+ax[0,0].imshow(lennar, cmap = 'Greys_r')
+ax[0,0].set_title('Rotkanal')
+
+ax[0,1].imshow(lennag, cmap = 'Greys_r')
+ax[0,1].set_title('Gruenkanal')
+
+ax[0,2].imshow(lennab, cmap = 'Greys_r')
+ax[0,2].set_title('Blaukanal')
+
+ax[1,0].imshow(lennainv)
+ax[1,0].set_title('RGB invertiert')
+
+ax[1,1].imshow(lenna)
+ax[1,1].set_title('Original')
+
+#6. Achsen muessen ausgewaehlt werden
+rgbmean = np.mean(lenna, axis = (0,1))
+rgbstd = np.std(lenna, axis = (0,1))
+
+#Aufg. 2
+
+d = np.load('./trainingsDatenFarbe2.npz')
+trImgs = d['data']
+trLabels = d['labels']
+
+valD = np.load('./validierungsDatenFarbe2.npz')
+valImgs = valD['data']
+valLabels = valD['labels'] 
+
+def Aufg31Farbe():
+    trMeans = []
+    for count in range(trImgs.shape[0]):
+        trMeans.append(np.mean(trImgs[count,:,:], axis = (0,1)))
+    
+    valMeans = []
+    for count in range(valImgs.shape[0]):
+        valMeans.append(np.mean(valImgs[count,:,:], axis = (0,1)))
+        
+    labels = []
+    
+    for count in range(len(valMeans)):
+        dists = []    
+        for i in range(len(trMeans)):
+            dists.append(np.linalg.norm(trMeans[i]-valMeans[count]))
+        labels.append(trLabels[dists.index(min(dists))])
+    
+    return labels  
+
+print np.sum(valLabels==Aufg31Farbe())
+print np.sum(valLabels==Aufg31Farbe()) / float(valImgs.shape[0]) * 100
+
+#50% Genauigkeit, also deutlich besseres Ergebnis
+
+#Aufg. 3
+
+def Aufg32Farbe(bins):
+    
+    trHists = []
+    
+    for count in range(trImgs.shape[0]):
+        img = trImgs[count,:,:,:]
+        rotKanal = img[:,:,0]
+        gruenKanal = img[:,:,1]
+        blauKanal = img [:,:,2]
+        trHists.append((makeHist(rotKanal, bins), makeHist(blauKanal, bins), makeHist(gruenKanal, bins)))
+    
+    valHists = []
+    
+    for count in range(valImgs.shape[0]):
+        img = valImgs[count,:,:,:]
+        rotKanal = img[:,:,0]
+        gruenKanal = img[:,:,1]
+        blauKanal = img [:,:,2]
+        valHists.append((makeHist(rotKanal, bins), makeHist(blauKanal, bins), makeHist(gruenKanal, bins)))
+    
+    labels = []
+    
+    for count in range(len(valHists)):
+        dists = []    
+        for i in range(len(trHists)):
+            dists.append(np.linalg.norm(np.asarray(trHists[i])-np.asarray(valHists[count])))
+        labels.append(trLabels[dists.index(min(dists))])
+        
+    return labels
+
+def makeHist(img, nrBins):
+    return np.histogram(img, bins = nrBins)[0]
+
+#Das kann ein bisschen dauern je nach PC
+def optimalBins():
+    percentages = []
+    for i in range(1,256):
+        percentages.append(Aufg32Farbe(i))
+    return max(percentages), percentages.index(max(percentages))
+
+print optimalBins()
+print np.sum(valLabels==Aufg32Farbe(219))
+print np.sum(valLabels==Aufg32Farbe(219)) / float(valImgs.shape[0]) * 100
+
+#Optimales Ergebnis is 56.666...% mit 219 bins
+#Wenn ich mich richtig erinnere nicht viel besser als mit Graustufenhistogrammen
+    
+    
