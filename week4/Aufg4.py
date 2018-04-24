9dc8bfb941b210146cc1cab8af5aca2569419e30
Henrik <henrik.peters.98@web.de>
2018-04-24 09:41:09 +0200

    preparations for week 4

---
+++ week4/Aufg4.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 20 15:40:51 2018
+
+@author: Moritz Lahann(6948050), Henrik Peters(6945965), Michael Huang(6947879)
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.io import imread
+import timeit
+
