1df46c1745b7a45de270a9965f90281824076c44
AgeInPee <henrik.peters.98@web.de>
2018-04-26 23:39:01 +0200

    improved classify_means with numpy functions instead of map which was in there because it was supposed to improve for-loops.

--- week4/Aufg4.py
+++ week4/Aufg4.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Apr 26 16:24:44 2018
 
-@author: 6peters
+@author: Moritz Lahann(6948050), Henrik Peters(6945965), Michael Huang(6947879)
 """
 
 import numpy as np
@@ -14,33 +14,48 @@ from skimage import transform
 val_imgs = glob.glob('./haribo1/hariboVal/*.png')
 tr_imgs = glob.glob('./haribo1/hariboTrain/*.png')
 
-val_labels = [x.split('/')[-1].split('.')[0].split('_')[0] for x in val_imgs]
-tr_labels = [x.split('/')[-1].split('.')[0].split('_')[0] for x in tr_imgs]
-    
-#general helper functions
+val_labels = [x.split('/')[-1].split('.')[0].split('_')[0].split('\\')[1] for x in val_imgs]
+tr_labels = [x.split('/')[-1].split('.')[0].split('_')[0].split('\\')[1] for x in tr_imgs]
+
+
+# general helper functions
 def load_imgs():
     for i,img in enumerate(val_imgs):
         val_imgs[i] = imread(img)
     for i,img in enumerate(tr_imgs):
         tr_imgs[i] = imread(img)
-        
-        
+    print val_imgs
+    print tr_imgs
+    print val_labels
+    print tr_labels
+    print create_seperator()
+
+
+def create_seperator():
+    sep = ''
+    for i in range(15):
+        sep += '---------'
+    return sep
+
+
 def compare_correct(labels):
     count = 0
     for i,label in enumerate(labels):
         if val_labels[i] == label:
             count += 1
     return count
-        
-        
+
+
+def curry (prior, *additional):
+    def curried(*args):
+        return prior(*(args + additional))
+    return curried
+
+
 #task functions
 def classify_means():
-    val_means = []
-    tr_means = []
-    for img in val_imgs:
-        val_means.append(np.mean(img, axis=(0, 1)))
-    for img in tr_imgs:
-        tr_means.append(np.mean(img, axis=(0, 1)))
+    val_means = np.mean(np.array(val_imgs), axis=(1,2))
+    tr_means = np.mean(np.array(tr_imgs), axis=(1,2))
     return val_means, tr_means
     
 
@@ -55,7 +70,6 @@ def compare_means():
         for i in range(len(tr_means)):
             dists.append(np.linalg.norm(tr_means[i] - val_means[count]))
         labels.append(tr_labels[np.argmin(dists)])
-
     return labels
     
     
@@ -69,7 +83,8 @@ def classify_3dhists():
         img = img.reshape((img.shape[0]*img.shape[1],3))
         tr_hists.append(np.histogramdd(img, bins = [8,8,8], range=((0,256),(0,256),(0,256)))[0])
     return val_hists, tr_hists
-        
+
+
 def compare_3dhists():
     hists = classify_3dhists()
     val_hists = hists[0]
@@ -82,8 +97,7 @@ def compare_3dhists():
             dists.append(np.linalg.norm(tr_hists[i] - val_hists[count]))
         labels.append(tr_labels[np.argmin(dists)])
 
-    return labels      
-    
+    return labels
     
 
 if __name__ == '__main__':
@@ -93,4 +107,3 @@ if __name__ == '__main__':
     print compare_correct(compare_means()), 'Labels were chosen correct'
     print compare_3dhists()
     print compare_correct(compare_3dhists()), 'Labels were chosen correct'
-    
