8ba4f229ab787e455fa5151f2fd125d666252b83
AgeInPee <henrik.peters.98@web.de>
2018-04-27 12:10:52 +0200

    task 2.1 only bounding boxes missing. task 2.2 otsu complete

--- week4/Aufg4.py
+++ week4/Aufg4.py
@@ -6,10 +6,11 @@ Created on Thu Apr 26 16:24:44 2018
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as plt
 from skimage.io import imread
 import glob
-from skimage import transform
+from skimage.filters import threshold_otsu
+import itertools
 
 val_imgs = glob.glob('./haribo1/hariboVal/*.png')
 tr_imgs = glob.glob('./haribo1/hariboTrain/*.png')
@@ -24,11 +25,6 @@ def load_imgs():
         val_imgs[i] = imread(img)
     for i,img in enumerate(tr_imgs):
         tr_imgs[i] = imread(img)
-    print val_imgs
-    print tr_imgs
-    print val_labels
-    print tr_labels
-    print create_seperator()
 
 
 def create_seperator():
@@ -46,13 +42,39 @@ def compare_correct(labels):
     return count
 
 
-def curry (prior, *additional):
-    def curried(*args):
-        return prior(*(args + additional))
-    return curried
+def to_binary_hsv(img, lower, upper):
+    return (lower < img) & (img < upper)
 
 
-#task functions
+def to_binary_rgb(img, value):
+    return img < value
+
+
+def show_imgs_hsv(imgs, subplot_x, subplot_y):
+    fig, ax = plt.pyplot.subplots(subplot_x, subplot_y)
+
+    for i in range(subplot_x):
+        for j in range(subplot_y):
+            if(imgs[i] is None):
+                break
+            else:
+                ax[i, j].imshow(plt.colors.hsv_to_rgb(imgs[i+j]), 'Greys_r')
+    plt.pyplot.show(block=True)
+
+
+def show_imgs_rgb(imgs, subplot_x, subplot_y):
+    fig, ax = plt.pyplot.subplots(subplot_x, subplot_y)
+
+    for i in range(subplot_x):
+        for j in range(subplot_y):
+            if(imgs[i] is None):
+                break
+            else:
+                ax[i, j].imshow(imgs[i+j], 'Greys_r')
+    plt.pyplot.show(block=True)
+
+
+# task functions
 def classify_means():
     val_means = np.mean(np.array(val_imgs), axis=(1,2))
     tr_means = np.mean(np.array(tr_imgs), axis=(1,2))
@@ -98,12 +120,55 @@ def compare_3dhists():
         labels.append(tr_labels[np.argmin(dists)])
 
     return labels
-    
+
+
+def convert_to_hsv():
+    val_imgs_hsv = map(plt.colors.rgb_to_hsv, val_imgs)
+    tr_imgs_hsv = map(plt.colors.rgb_to_hsv, tr_imgs)
+    return val_imgs_hsv, tr_imgs_hsv
+
+
+def binarize():
+    imgs_hsv = convert_to_hsv()
+    val_imgs_hsv = imgs_hsv[0]
+    tr_imgs_hsv = imgs_hsv[1]
+
+    val_binary = list(itertools.starmap(to_binary_hsv, [(img, 30, 140) for img in val_imgs_hsv]))
+    tr_binary = list(itertools.starmap(to_binary_hsv, [(img, 30, 140) for img in tr_imgs_hsv]))
+    val_binary = [255*img for img in val_binary]
+    tr_binary = [255*img for img in tr_binary]
+
+    return val_binary, tr_binary
+
+
+def binarize_otsu():
+    val_imgs_greys = list(itertools.starmap(np.dot, [(img, [0.299, 0.587, 0.114]) for img in val_imgs]))
+    tr_imgs_greys = list(itertools.starmap(np.dot, [(img, [0.299, 0.587, 0.114]) for img in tr_imgs]))
+    val_otsu = map(threshold_otsu, val_imgs_greys)
+    tr_otsu = map(threshold_otsu, tr_imgs_greys)
+
+    val_binary_otsu = list(itertools.starmap(to_binary_rgb,
+                                             [(img, val_otsu[i]) for i,img in enumerate(val_imgs_greys)]))
+    tr_binary_otsu = list(itertools.starmap(to_binary_rgb,
+                                            [(img, tr_otsu[i]) for i,img in enumerate(tr_imgs_greys)]))
+    val_binary_otsu = [255*img for img in val_binary_otsu]
+    tr_binary_otsu = [255*img for img in tr_binary_otsu]
+
+    return val_binary_otsu, tr_binary_otsu
+
+
+def create_bounding_boxes():
+    return 'bounding boxes still have to be created'
+
+
 
 if __name__ == '__main__':
     load_imgs()
-    print val_labels
-    print compare_means()
-    print compare_correct(compare_means()), 'Labels were chosen correct'
-    print compare_3dhists()
-    print compare_correct(compare_3dhists()), 'Labels were chosen correct'
+    #print val_labels
+    #print compare_means()
+    #print compare_correct(compare_means()), 'Labels were chosen correct'
+    #print compare_3dhists()
+    #print compare_correct(compare_3dhists()), 'Labels were chosen correct'
+    #print create_seperator()
+    #show_imgs_hsv(binarize()[1], 3, 4)
+    create_bounding_boxes()
