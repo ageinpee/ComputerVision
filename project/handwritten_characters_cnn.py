1bc128e0ae81c4b6140b16f1e445fbbe864a03d0
Henrik Peters <6peters@informatik.uni-hamburg.de>
2018-06-14 16:40:01 +0200

    first cnn

--- project/handwritten_characters_cnn.py
+++ project/handwritten_characters_cnn.py
@@ -15,18 +15,99 @@ Imports:
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 import keras
+
+import image_loader
 
 
 """
 Helping functions:
 --------------------------------------------------------------------------
 """
+def to_binary(img, upper):
+    return (img < upper)
 
+def compile_cnn(X_train):
+    model = keras.Sequential()
+    
+    model.add(keras.layers.Conv2D(32, (3,3), activation='relu', padding='same', 
+                     input_shape=(X_train.shape[1:]), name='conv1'))
+    model.add(keras.layers.Conv2D(32, (3,3), activation='relu', padding='same', 
+                     input_shape=(X_train.shape[1:]), name='conv2'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
+    model.add(keras.layers.Conv2D(64, (3,3), activation='relu', padding='same', 
+                     input_shape=(X_train.shape[1:]), name='conv21'))
+    model.add(keras.layers.Conv2D(64, (3,3), activation='relu', padding='same', 
+                     input_shape=(X_train.shape[1:]), name='conv22'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(2,2)))
+    model.add(keras.layers.Flatten())
+    
+    model.add(keras.layers.Dense(256, activation='relu', name='fc1'))
+    model.add(keras.layers.Dense(26, activation='softmax', name='output'))
+    
+    model.compile(loss='categorical_crossentropy', 
+                      optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9), 
+                      metrics=['accuracy'])
+    
+    return model
 
-
+def train_cnn(model, X_train, Y_train):
+    model.fit(X_train, Y_train, batch_size=32, epochs=20,
+          validation_split = 0.2, verbose=1, 
+          callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', 
+                                                  min_delta=0, patience=3), 
+                    keras.callbacks.ModelCheckpoint(filepath='./project.h5', monitor='val_loss', 
+                                                    verbose=0, save_best_only=True, 
+                                                    save_weights_only=False, 
+                                                    mode='auto', period=1)])
 """
 Main functions:
 --------------------------------------------------------------------------
 """
+
+if __name__ == '__main__':
+    images = image_loader.get_images("data/Data_Processed")
+    for key in images:
+        print len(images[key])
+    X_train = []
+    Y_train = []
+    
+    X_test = []
+    Y_test = []
+    for key, i in enumerate(images):
+        for j in range(0, trainmin):
+            rnd_img = random.choice(images[key])
+            X_train.append(rnd_img)
+            images[key] = images[key].remove(rnd_img)
+            Y_train.append(i)
+        for j in range(0, testmin):
+            rnd_img = random.choice(images[key])
+            X_test.append(rnd_img)
+            images[key] = images[key].remove(rnd_img)
+            Y_test.append(i)
+        
+    #binarize images
+    for img in X_train:
+        to_binary(img, 255)
+    for img in X_test:
+        to_binary(img, 255)
+    
+    #convert to suitable for CNN
+    Y_train = keras.utils.to_categorical(Y_train, 26)
+    X_train = X_train.astype(np.float32)/255
+    Y_test = keras.utils.to_categorical(Y_test, 26)
+    X_test = X_test.astype(np.float32)/255
+    
+    #only run for new parameters
+    model = compile_cnn(X_train)    
+    train_cnn(model, X_train, Y_train)
+    
+    model.load_weights('./W7.h5', by_name=True)
+
+    score = model.evaluate(X_test, Y_test, verbose=1)    
+    
+    print score[1]
+    plt.imshow(images["A"][0])  # test
+    plt.show()
--- project/image_loader.py
+++ project/image_loader.py
@@ -11,7 +11,7 @@ This file is supposed to load the training and evaluation images from the .7z fi
 Imports:
 --------------------------------------------------------------------------
 """
-import imageio
+import skimage.io
 import os
 
 """
@@ -36,10 +36,11 @@ def print_progress_bar (iteration, total, prefix='', suffix='', decimals=1, leng
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filled_length = int((length * iteration) / total)
     bar = fill * filled_length + '--' * (length - filled_length)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end=" ")
+    print '\r{0} |{1}| {2} {3}'.format(prefix, bar, percent, suffix),
+    #('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end=" ")
     # Print New Line on Complete
     if iteration == total:
-        print()
+        print " "
 
 
 """
@@ -49,7 +50,6 @@ Main functions:
 
 
 def get_images(file_path):
-    files = []
     letters = {"A": [], "B": [], "C": [], "D": [], "E": [], "F": [],
                "G": [], "H": [], "I": [], "J": [], "K": [], "L": [],
                "M": [], "N": [], "O": [], "P": [], "Q": [], "R": [],
@@ -62,59 +62,59 @@ def get_images(file_path):
         if filename.endswith(".png"):
             first, second, third = filename.split("_")
             if second == "65":
-                letters["A"].append(imageio.imread(file_path + "/" + filename))
+                letters["A"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "66":
-                letters["B"].append(imageio.imread(file_path + "/" + filename))
+                letters["B"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "67":
-                letters["C"].append(imageio.imread(file_path + "/" + filename))
+                letters["C"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "68":
-                letters["D"].append(imageio.imread(file_path + "/" + filename))
+                letters["D"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "69":
-                letters["E"].append(imageio.imread(file_path + "/" + filename))
+                letters["E"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "70":
-                letters["F"].append(imageio.imread(file_path + "/" + filename))
+                letters["F"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "71":
-                letters["G"].append(imageio.imread(file_path + "/" + filename))
+                letters["G"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "72":
-                letters["H"].append(imageio.imread(file_path + "/" + filename))
+                letters["H"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "73":
-                letters["I"].append(imageio.imread(file_path + "/" + filename))
+                letters["I"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "74":
-                letters["J"].append(imageio.imread(file_path + "/" + filename))
+                letters["J"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "75":
-                letters["K"].append(imageio.imread(file_path + "/" + filename))
+                letters["K"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "76":
-                letters["L"].append(imageio.imread(file_path + "/" + filename))
+                letters["L"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "77":
-                letters["M"].append(imageio.imread(file_path + "/" + filename))
+                letters["M"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "78":
-                letters["N"].append(imageio.imread(file_path + "/" + filename))
+                letters["N"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "79":
-                letters["O"].append(imageio.imread(file_path + "/" + filename))
+                letters["O"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "80":
-                letters["P"].append(imageio.imread(file_path + "/" + filename))
+                letters["P"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "81":
-                letters["Q"].append(imageio.imread(file_path + "/" + filename))
+                letters["Q"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "82":
-                letters["R"].append(imageio.imread(file_path + "/" + filename))
+                letters["R"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "83":
-                letters["S"].append(imageio.imread(file_path + "/" + filename))
+                letters["S"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "84":
-                letters["T"].append(imageio.imread(file_path + "/" + filename))
+                letters["T"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "85":
-                letters["U"].append(imageio.imread(file_path + "/" + filename))
+                letters["U"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "86":
-                letters["V"].append(imageio.imread(file_path + "/" + filename))
+                letters["V"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "87":
-                letters["W"].append(imageio.imread(file_path + "/" + filename))
+                letters["W"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "88":
-                letters["X"].append(imageio.imread(file_path + "/" + filename))
+                letters["X"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "89":
-                letters["Y"].append(imageio.imread(file_path + "/" + filename))
+                letters["Y"].append(skimage.io.imread(file_path + "/" + filename))
             elif second == "90":
-                letters["Z"].append(imageio.imread(file_path + "/" + filename))
+                letters["Z"].append(skimage.io.imread(file_path + "/" + filename))
 
-        printProgressBar(i+1, length, prefix='Progress:', suffix='Complete', length=50)
+        print_progress_bar(i+1, length, prefix='Progress:', suffix='Complete', length=50)
         #tempcount += 1  # <<<<<<<<<<<<<<<<<<<<<<<<<
         #if tempcount == 20:     # <<<<<<<<<<<<<<<<<<<<<<<<<
          #   break   # <<<<<<<<<<<<<<<<<<<<<<<<<
