fixed rotation, added new dataset

project/handwritten_characters_cnn.py
@@ -39,11 +39,15 @@ def to_binary(img, value):
 def compile_cnn(X_train):
     model = keras.Sequential()
     print(X_train[0].shape)
-    model.add(keras.layers.Conv2D(32, (10, 10), activation='relu', padding='same',
-                                  input_shape=(28, 28, 1), name='conv1'))
-    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(keras.layers.Conv2D(32, (10, 10), activation='relu', padding='same',
+    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same',
+                                  input_shape=X_train.shape[1:], name='conv1'))
+    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same',
                                   name='conv2'))
+    model.add(keras.layers.MaxPooling2D(pool_size=(3, 3)))
+    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same',
+                                  name='conv3'))
+    model.add(keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same',
+                                  name='conv4'))
     model.add(keras.layers.MaxPooling2D(pool_size=(3, 3)))
     model.add(keras.layers.Flatten())
     model.add(keras.layers.Dense(256, activation='relu', name='fc1'))
@@ -78,6 +82,7 @@ def compile_cnn(X_train):
     """
 
 def train_cnn(model, X_train, Y_train):
+    '''
     model.fit(X_train, Y_train, batch_size=32, epochs=200,
           validation_split = 0.2, verbose=1, 
           callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', 
@@ -86,6 +91,9 @@ def train_cnn(model, X_train, Y_train):
                                                     verbose=0, save_best_only=True, 
                                                     save_weights_only=False, 
                                                     mode='auto', period=1)])
+    '''
+    model.fit(X_train, Y_train, batch_size=32, epochs=20,
+          validation_split = 0.2, verbose=1)
 """
 Main functions:
 --------------------------------------------------------------------------
@@ -93,7 +101,8 @@ Main functions:
 
 if __name__ == '__main__':
     images = image_ops.load_images_npz(input("Enter a file path for the data: "))
-
+    #images = np.load('C:/Users/Moritz Lahann/Desktop/STUDIUM/PRAKTIKUM COMPUTERVISION/DATA/TEST.npz')
+    '''
     for key in images:
         images[key] = np.reshape(images[key][:,:,:,0], newshape=(1000,28,28,1))
         images[key] = to_binary(images[key], 127)*255
@@ -117,29 +126,43 @@ if __name__ == '__main__':
 
     print(X_train.shape)
     print(X_test.shape)
-
-    print(X_train)
+    '''
+    
+    keylen = images["A"].shape[0]
+    print(keylen)
+    
+    X_train = np.zeros(shape=(26*keylen, 28, 28, 4))
+    Y_train = []
+    
+    for i, key in enumerate(images):
+        for j in range(keylen):
+            X_train[i*keylen+j] = images[key][j]
+            Y_train.append(key)
+    
+    #print(X_train)
     for i in range(len(Y_train)):
         Y_train[i] = ord(Y_train[i])-65
     print(Y_train)
+    '''
     for i in range(len(Y_test)):
         Y_test[i] = ord(Y_test[i])-65
     print(Y_test)
-    
+    '''
     #convert to suitable for CNN
     Y_train = keras.utils.to_categorical(Y_train, 26)
-    Y_test = keras.utils.to_categorical(Y_test, 26)
+    #Y_test = keras.utils.to_categorical(Y_test, 26)
 
     X_train = X_train.astype(np.float32)/255
-    X_test = X_test.astype(np.float32)/255
+    #X_test = X_test.astype(np.float32)/255
 
 
     #only run for new parameters
     model = compile_cnn(X_train)
     train_cnn(model, X_train, Y_train)
     
-    model.load_weights('./project.h5', by_name=True)
+    #model.load_weights('./project.h5', by_name=True)
 
+    '''
     score = model.evaluate(X_test, Y_test, verbose=1)
     
     print(score[1])
@@ -158,3 +181,4 @@ if __name__ == '__main__':
     print(label_print)
     #plt.imshow(X_train[4000], "Greys_r")  # test
     plt.show()
+    '''

project/image_ops.py
@@ -94,7 +94,7 @@ def augment_images(images, wanted_length, label='unknown'):
         for i in range(wanted_length-len(images)):
             choice = random.choice(images)
             choice = scipy.ndimage.interpolation.rotate(choice, float(random.choice([-10, -8.5, -7, -6, -5, -2,
-                                                                                     2, 5, 6, 7, 8.5, 10])), reshape=False)
+                                                                                     2, 5, 6, 7, 8.5, 10])), reshape=False, mode='constant', cval=255)
             out_images.append(choice)
             #print_progress_bar(i, wanted_length-len(images), prefix='augmenting images for label {0}:'.format(label), suffix='Complete', length=50)
     print('')
@@ -216,3 +216,13 @@ saves a list of ndarrays as a .npz compressed-numpy-file
 """
 def save_images_npz(file_path, images):
     np.savez_compressed(file_path, images)
+    
+def test():
+    images = load_images(input("Enter a file path for the images: "))
+    aug_imgs = {}
+    aug_imgs.fromkeys(images.keys(), [])
+    for key in images:
+        aug_imgs[key] = augment_images(images[key], 2000, label=key)
+    show_images(aug_imgs["F"][1000:2000], 30, 30)
+    for key in aug_imgs:
+        save_images_npz('C:/Users/Moritz Lahann/Desktop/STUDIUM/PRAKTIKUM COMPUTERVISION/DATA/TEST/' + key, aug_imgs[key])